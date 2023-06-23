f47da5b8b3812fd0969da8435073e11cd79b086a
venastia <nastiavenastia@gmail.com>
2023-06-23 12:31:55 +0300

    -

--- js/order.js
+++ js/order.js
@@ -1,56 +1,57 @@
-var imageContainers1 = Array.from(document.getElementsByClassName('image-container'));
-var texts1 = Array.from(document.getElementsByClassName('text'));
+$(document).ready(function() {
+  var imageContainers1 = $('.image-container');
+  var texts1 = $('.text');
 
-for (let i = 0; i < imageContainers1.length; i++) {
-  let images = Array.from(imageContainers1[i].getElementsByClassName('gallery-image'));
-  let currentImageIndex = 0;
+  imageContainers1.each(function(i) {
+    var images = $(this).find('.gallery-image');
+    var currentImageIndex = 0;
 
-  function switchImage() {
-    images[currentImageIndex].style.display = 'none';
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    images[currentImageIndex].style.display = 'block';
-  }
+    function switchImage() {
+      images.eq(currentImageIndex).hide();
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      images.eq(currentImageIndex).show();
+    }
 
-  imageContainers1[i].addEventListener('mouseover', function() {
-    texts1[i].style.display = 'block';
-    images[currentImageIndex].setAttribute('clip-path', 'url(#mask2)');
+    $(this).on('mouseover', function() {
+      texts1.eq(i).show();
+      images.eq(currentImageIndex).attr('clip-path', 'url(#mask2)');
+    });
+
+    $(this).on('mouseout', function() {
+      texts1.eq(i).hide();
+      images.eq(currentImageIndex).attr('clip-path', 'url(#mask1)');
+    });
+
+    $(this).on('click', function() {
+      switchImage();
+    });
   });
 
-  imageContainers1[i].addEventListener('mouseout', function() {
-    texts1[i].style.display = 'none';
-    images[currentImageIndex].setAttribute('clip-path', 'url(#mask1)');
+  var imageContainers2 = $('.image-container2');
+  var texts2 = $('.text2');
+
+  imageContainers2.each(function(i) {
+    var images = $(this).find('.gallery-image2');
+    var currentImageIndex = 0;
+
+    function switchImage() {
+      images.eq(currentImageIndex).hide();
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      images.eq(currentImageIndex).show();
+    }
+
+    $(this).on('mouseover', function() {
+      texts2.eq(i).show();
+      images.eq(currentImageIndex).attr('clip-path', 'url(#mask4)');
+    });
+
+    $(this).on('mouseout', function() {
+      texts2.eq(i).hide();
+      images.eq(currentImageIndex).attr('clip-path', 'url(#mask3)');
+    });
+
+    $(this).on('click', function() {
+      switchImage();
+    });
   });
-
-  imageContainers1[i].addEventListener('click', function() {
-    switchImage();
-  });
-}
-
-var imageContainers2 = Array.from(document.getElementsByClassName('image-container2'));
-var texts2 = Array.from(document.getElementsByClassName('text2'));
-
-for (let i = 0; i < imageContainers2.length; i++) {
-  let images = Array.from(imageContainers2[i].getElementsByClassName('gallery-image2'));
-  let currentImageIndex = 0;
-
-  function switchImage() {
-    images[currentImageIndex].style.display = 'none';
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-
-    images[currentImageIndex].style.display = 'block';
-  }
-
-  imageContainers2[i].addEventListener('mouseover', function() {
-    texts2[i].style.display = 'block';
-    images[currentImageIndex].setAttribute('clip-path', 'url(#mask4)');
-  });
-
-  imageContainers2[i].addEventListener('mouseout', function() {
-    texts2[i].style.display = 'none';
-    images[currentImageIndex].setAttribute('clip-path', 'url(#mask3)');
-  });
-
-  imageContainers2[i].addEventListener('click', function() {
-    switchImage();
-  });
-}
+});
